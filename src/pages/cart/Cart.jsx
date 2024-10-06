9a6a5528667ce7184e599e5dc217e1d00fb39018
aalimova <aalimova1517@gmail.com>
2024-10-05 20:02:09 -0500

    Updated the Cart Componenet

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 
  function Cart({cart}) {
+
+  const totalPrice = cart.reduce((sum, item) => sum +item.price, 0)
   return (
+    <>
     <div className="cart">
       <h2>Your Cart</h2>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
+        <>
         <ul>
           {cart.map((item, index) => (
             <li key={index}>
               <img src={item.thumbnail} alt={item.title} style={{ width: '50px', height: 'auto', marginRight: '10px' }} />
-              {item.title} - ${item.price}
+              <span>{item.title} - ${item.price}</span>
             </li>
           ))}
         </ul>
+       <h3>Total Price: ${totalPrice.toFixed(2)}</h3>
+       </>
       )}
     </div>
+    </>
   );
 }
 
